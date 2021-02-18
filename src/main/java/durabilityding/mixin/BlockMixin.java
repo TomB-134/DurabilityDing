354221a04cb6f9b0494cad2c86229fbfc6380c53
TomB_134 <Thomasbaum134@outlook.com>
2021-02-19 01:26:39 +1100

    Changed block break event inject to be called at TAIL rather than HEAD. Insures any item damaging logic takes place before DurabilityDing logic.

--- src/main/java/durabilityding/mixin/BlockMixin.java
+++ src/main/java/durabilityding/mixin/BlockMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class BlockMixin {
 
     //Simple hook into block break method that triggers an event.
-    @Inject(at = @At("HEAD"), method = "onBreak", cancellable = true) //Inject into block break method.
+    @Inject(at = @At("TAIL"), method = "onBreak", cancellable = true) //Inject into block break method.
     public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo info) {
         ActionResult result = BlockBreakCallback.EVENT.invoker().blockBreak(world,pos,state,player); //Call custom BlockBreakCallback event.
 
